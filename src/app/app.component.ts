5cc733513ed362ec49df2f6733d5a4ee139ee9d8
Aung Khant <theaungkhant@gmail.com>
2017-02-27 12:45:58 +0000

    Cat Editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
 
+export class Cat{
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   template:`
   <h1>{{title}}</h1>
+  <h2>{{cat.name}} details!</h2>
+  <div><label>id: </label>{{cat.id}}</div>
+  <div><label>name: </label>{{cat.name}}</div>
+  <div>
+    <label>name: </label>
+    <input [(ngModel)]="cat.name" placeholder="cat's name">
+  </div>
   `,
 
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Our Lovely Cats!';
+
+  cat:Cat = {
+    id: 1,
+    name: 'Mr.Bunny'
+  };
 }
 
 
